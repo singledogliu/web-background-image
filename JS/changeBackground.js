134703dbce7818e23c7a4d4a83f96dbaf048e067
singledogliu <deqinliu@qq.com>
2020-02-06 17:12:10 +0800

    更改图片地址输入功能代码。取消单地址输入框和多地址输入框，改为一输入框以图片地址，单击一次加号添加一个输入框。

--- JS/changeBackground.js
+++ JS/changeBackground.js
@@ -1,10 +1,12 @@
-var imgURL = "";
 var defaultImgURL = "http://r.photo.store.qq.com/psb?/V13L31cV41nzmD/GfPRm3GCjN8x1R4wjYALzt3151eNhdbPskiZ4VntyFc!/r/dPQAAAAAAAAA";
-var imgsURL = [];
 var defaultOpacity = 0.1;
 var opacity = 0.1;
 var interval_time = 3;
 var orderIndex = 0;       //顺序更换图片时图片地址在数组中的位置
+// 图片输入框内容组对象
+inputs = new Object();
+// 图片对象
+input = new Object();
 
 jQuery(document).ready(function ($) {
 
@@ -26,25 +28,19 @@ jQuery(document).ready(function ($) {
     // 读取本地设置数据
     function getStorage() {
         chrome.storage.sync.get({
-            imgURL_storage: "", imgsURL_storage: [], opacity_storage: 0.1, interval_time_storage: 1, replacement_mode_storage: "order"
+            imgURL_storage: "", imgsURL_storage: [], opacity_storage: 0.1, interval_time_storage: 1,
+            inputs_count_storage: 1, replacement_mode_storage: "order", inputs_storage: inputs,
         }, function (items) {
-            if (items.imgURL_storage != 0) {
-                imgURL = items.imgURL_storage;
-            }
-            else {
-                imgURL = "";
+            // 输入框对象的key组
+            let keys = Object.keys(items.inputs_storage);
+            if (keys.length > 0) {
+                inputs = items.inputs_storage;
             }
             if (items.opacity_storage != "" && items.opacity_storage != null) {
                 opacity = items.opacity_storage;
             }
             if (items.interval_time_storage != "" && items.interval_time_storage != null) {
                 interval_time = items.interval_time_storage;
-            }
-            if (items.imgsURL_storage != "" && items.imgsURL_storage != null && items.imgsURL_storage != 0) {
-                imgsURL = items.imgsURL_storage;
-            }
-            else {
-                imgsURL = [];
             }
             replacement_mode = items.replacement_mode_storage;
             setBackground();
@@ -56,8 +52,10 @@ jQuery(document).ready(function ($) {
 
     // 修改背景
     function setBackground() {
-        var img = imgsURL;
+        // var img = imgsURL;
         var interval_time_true = 3;
+        let keys = Object.keys(inputs);
+
         // 输入数据(更换壁纸时间)合法性检测
         if (interval_time > 0) {
             interval_time_true = interval_time * 60000;
@@ -71,56 +69,51 @@ jQuery(document).ready(function ($) {
         else {
             bac.style.opacity = defaultOpacity;
         }
-        // 判断应该设置单张背景还是多张轮播
-        if (imgURL === "" || imgURL === null) {
-            if (img.length === 0) {
-                setBackgroundImage(defaultImgURL);
-            }
-            else {
-                changeBackgroundImage();
-                //定时（interval_time_true分钟）更换背景图片
-                task1 = setInterval(function () {
-                    changeBackgroundImage();
-                    if (replacement_mode === "order") {
-                        orderIndex++;
-                        if (orderIndex == img.length) {
-                            orderIndex = 0;
-                        }
-                    }
-                }, interval_time_true);
-            }
+        /** 判断应该设置单张背景还是多张轮播*/
+        //只有一个图片地址
+        if (keys.length === 1) {
+            setBackgroundImage(inputs[keys[0]].url);
         }
         else {
-            setBackgroundImage(imgURL);
+            changeBackgroundImage();
+            //定时（interval_time_true分钟）更换背景图片
+            task1 = setInterval(function () {
+                changeBackgroundImage();
+                if (replacement_mode === "order") {
+                    orderIndex++;
+                    if (orderIndex == keys.length) {
+                        orderIndex = 0;
+                    }
+                }
+            }, interval_time_true);
         }
-
-        // 更换一张壁纸(顺序or随机)
-        function changeBackgroundImage() {
-            var Iimg = "";
-            var serial_number = "";
-            // 随机更换
-            if (replacement_mode === "random") {
-                serial_number = Math.floor(Math.random() * img.length);
-            }
-            // 顺序更换
-            else {
-                serial_number = orderIndex;
-            }
-
-            Iimg = img[serial_number];
-            setBackgroundImage(Iimg);
-        }
-
-        // 设置单张背景
-        function setBackgroundImage(imageURL) {
-            bac.style.background = "url(" + imageURL + ")";
-            bac.style.backgroundPosition = "50% 50%";
-            bac.style.backgroundRepeat = "no-repeat";
-        }
-
     }
 
-  
+    // 更换一张壁纸(顺序or随机)
+    function changeBackgroundImage() {
+        var Iimg = "";
+        var serial_number = "";
+        let keys = Object.keys(inputs);
+        // 随机更换
+        if (replacement_mode === "random") {
+            serial_number = Math.floor(Math.random() * keys.length);
+        }
+        // 顺序更换
+        else {
+            serial_number = orderIndex;
+        }
+        Iimg = inputs[keys[serial_number]].url;
+        setBackgroundImage(Iimg);
+    }
+
+    // 设置单张背景
+    function setBackgroundImage(imageURL) {
+        bac.style.background = "url(" + imageURL + ")";
+        bac.style.backgroundPosition = "50% 50%";
+        bac.style.backgroundRepeat = "no-repeat";
+    }
+
+
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         if (request.cmd === 'reload') {
             if (task1 !== "") {
@@ -128,9 +121,9 @@ jQuery(document).ready(function ($) {
             }
             getStorage();
         }
-        // if (request.cmd === 'guide') {
-        //     goToGuidePage();
-        // }
+        if (request.cmd === 'warn') {
+            alert(request.message);
+        }
         // sendResponse('我收到了你的消息！');
     });
 
--- JS/popup.js
+++ JS/popup.js
@@ -1,17 +1,31 @@
-var imgURL = "";
+// 默认图片地址
 var defaultImgURL = "http://r.photo.store.qq.com/psb?/V13L31cV41nzmD/GfPRm3GCjN8x1R4wjYALzt3151eNhdbPskiZ4VntyFc!/r/dPQAAAAAAAAA";
-var imgsURL = [];
-var defaultImgsURL = [];
+// 默认透明度
 var defaultOpacity = 0.1;
+// 透明度
 var opacity = 0.1;
+// 默认图片切换间隔时间
 var defaultInterval_time = 3;
+// 图片切换间隔时间
 var interval_time = 3;
-let replacement_mode = "";
-let default_replacement_mode = "random";
+// 图片切换方式
+let replacement_mode = "order";
+// 默认图片切换方式
+let default_replacement_mode = "order";
+// 图片输入框的数量
+let inputs_count = 1;
+// 图片输入框内容组对象
+inputs = new Object();
+// 图片对象
+input = new Object();
 
+input.url = defaultImgURL;
+inputs.image0 = input;
+
+// backgroundPage
 var bg = chrome.extension.getBackgroundPage();
 
-// 给ContentScript发消息
+/**给ContentScript发消息 */
 function sendMessageToContentScript(message) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
@@ -20,12 +34,11 @@ function sendMessageToContentScript(message) {
     });
 }
 
-// 多语言支持
+/**多语言支持 */
 function localizeHtmlPage() {
     var objects = document.getElementsByTagName('html');
     for (var j = 0; j < objects.length; j++) {
         var obj = objects[j];
-
         var valStrH = obj.innerHTML.toString();
         var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function (match, v1) {
             return v1 ? chrome.i18n.getMessage(v1) : "";
@@ -37,17 +50,50 @@ function localizeHtmlPage() {
     }
 }
 
+/**将输入框数量存入localstorage */
+function setInputsCountToLocalStroage(inputs_count_storage) {
+    chrome.storage.sync.set({
+        inputs_count_storage: inputs_count_storage
+    },
+        function () {
+        });
+}
+
+/**删除输入框 */
+function deleteEvent(e) {
+    if (Object.keys(inputs).length === 1) {
+        sendMessageToContentScript({ cmd: "warn", message: chrome.i18n.getMessage("delete_tip") });
+        // alert(chrome.i18n.getMessage("delete_tip"));
+    }
+    else {
+        let key = e.path[1].id;
+        e.path[2].removeChild(e.path[1]);
+        inputs_count--;
+        delete inputs[e.path[1].id];
+        setInputsCountToLocalStroage(inputs_count);
+        chrome.storage.sync.set({
+            inputs_storage: inputs
+        },
+            function () {
+            });
+    }
+}
+
 jQuery(document).ready(function ($) {
-    // 读取本地设置数据
+    /**读取本地设置数据 */
     function getStorage() {
         chrome.storage.sync.get({
-            imgURL_storage: "", imgsURL_storage: [], opacity_storage: 0.1, interval_time_storage: 1, replacement_mode_storage: "order"
+            opacity_storage: 0.1, interval_time_storage: 1,
+            inputs_count_storage: 1, replacement_mode_storage: "order", inputs_storage: inputs,
         }, function (items) {
-            if (items.imgURL_storage != 0) {
-                imgURL = items.imgURL_storage;
+
+            // 输入框对象的key组
+            let keys = Object.keys(items.inputs_storage);
+            if (keys.length > 0) {
+                inputs = items.inputs_storage;
             }
-            else {
-                imgURL = "";
+            if (items.inputs_count_storage >= 1) {
+                inputs_count = items.inputs_count_storage;
             }
             if (items.opacity_storage != "" && items.opacity_storage != null) {
                 opacity = items.opacity_storage;
@@ -55,18 +101,10 @@ jQuery(document).ready(function ($) {
             if (items.interval_time_storage != "" && items.interval_time_storage != null) {
                 interval_time = items.interval_time_storage;
             }
-            $("#image").val(imgURL);
-            if (items.imgsURL_storage != "" && items.imgsURL_storage != null) {
-                imgsURL = items.imgsURL_storage;
-                $("#images").val(imgsURL.join(","));
-            }
-            else {
-                imgsURL = "";
-                $("#images").val(imgsURL);
-            }
             $("#setting_opacity").val(opacity);
             $("#interval_time").val(interval_time);
 
+            // 图片更换模式
             let nodes = document.getElementsByName("replacement_mode");
             let nodeLength = nodes.length;
             for (let i = 0; i < nodeLength; i++) {
@@ -76,7 +114,19 @@ jQuery(document).ready(function ($) {
             }
             document.getElementsByName("replacement_mode").innerHTML = nodes;
 
+            // 更新图片输入框
+            for (let i = 0; i < keys.length; i++) {
+                let element = document.createElement("div");
+                element.innerHTML = "<input type='text' value='" + inputs[keys[i]].url + "'><img src='/IMAGES/sub.png' alt='delete' class='delete'>"
+                element.className = "image";
+                element.setAttribute("id", "image" + i);
+                document.getElementById("images").insertBefore(element, document.getElementById("add"));
+                element.lastChild.addEventListener("click", function (e) {
+                    deleteEvent(e);
+                });
+            }
         });
+
     }
 
     // 获取设置数据
@@ -85,43 +135,84 @@ jQuery(document).ready(function ($) {
     // 根据浏览器语言修改显示语言
     localizeHtmlPage();
 
+    /**增加一个地址输入框 */
+    $("#add").on("click", function () {
+        inputs_count++;
+        let element = document.createElement("div");
+        element.innerHTML = "<input type='text'><img src='/IMAGES/sub.png' alt='delete' class='delete'>"
+        element.className = "image";
+        element.setAttribute("id", "image" + inputs_count - 1);
+        document.getElementById("images").insertBefore(element, document.getElementById("add"));
+        element.lastChild.addEventListener("click", function (e) {
+            deleteEvent(e);
+        });
+        setInputsCountToLocalStroage(inputs_count);
+    });
 
-    // 保存设置数据
+    /**保存设置数据 */
     $("#setting_btn").on("click", function () {
-        var imgURL_storage = $("#image").val();
-        var imgsURL_storage = $("#images").val();
+
+        let hasEmpty = false;
+
         var opacity_storage = $("#setting_opacity").val();
         var interval_time_storage = $("#interval_time").val();
         let replacement_mode_storage = "";
         let nodes = document.getElementsByName("replacement_mode");
         let ChildLength = nodes.length;
+        // 判断图片更换方式
         for (let i = 0; i < ChildLength; i++) {
             if (nodes[i].checked === true) {
                 replacement_mode_storage = nodes[i].value;
             }
         }
-        // 将设置数据保存到本地
-        chrome.storage.sync.set({
-                imgURL_storage: imgURL_storage,
-                imgsURL_storage: imgsURL_storage.split(","),
+
+        // 所有图片输入框的信息
+        let imagesInfo = document.getElementsByClassName("image");
+        for (let i = 0; i < imagesInfo.length; i++) {
+            if (imagesInfo[i].firstChild.value == "" || imagesInfo[i].firstChild.value == null) {
+                alert("有输入框为空，请输入内容或删除多余输入框");
+                hasEmpty = true;
+                break;
+            }
+            else {
+                input_local = new Object();
+                input_local.url = imagesInfo[i].firstChild.value;
+                inputs["image" + i] = input_local;
+                delete input_local;
+            }
+        }
+        // 没有空输入框，保存数据
+        if (hasEmpty === false) {
+            // 将设置数据保存到本地
+            chrome.storage.sync.set({
                 opacity_storage: opacity_storage,
                 interval_time_storage: interval_time_storage,
                 replacement_mode_storage: replacement_mode_storage,
+                inputs_storage: inputs,
             },
-            function () {
-                // $("#success").css("display", "block");
-                getStorage();
-                sendMessageToContentScript({ cmd: "reload" });
-            });
+                function () {
+                    sendMessageToContentScript({ cmd: "reload" });
+                });
+        }
+        else {
+            input = [];
+        }
     });
 
-    // 恢复默认设置
+    /**恢复默认设置 */
     $("#default_setting_btn").on("click", function () {
-        $("#image").val("");
+        // 清空inputs的属性
+        Object.keys(inputs).forEach(key => {
+            delete inputs[key];
+        });
+        // 输入框恢复默认
+        document.getElementById("images").innerHTML = "<img src='/IMAGES/add.png' alt='add' id='add'>";
+        // 初始化inputs
+        inputs.image0 = input;
         // 将设置数据保存到本地
         chrome.storage.sync.set({
-            imgURL_storage: 0,
-            imgsURL_storage: 0,
+            inputs_storage: inputs,
+            inputs_count_storage: 1,
             opacity_storage: defaultOpacity,
             interval_time_storage: defaultInterval_time
         },
@@ -132,22 +223,7 @@ jQuery(document).ready(function ($) {
             });
     });
 
-    //选择图片更换模式（顺序or随机）
-    $("#replacement_mode").on("click",function () {
-        // var replacement_mode = event.target.value;
-        // console.log(event.target.value);
-        // chrome.storage.sync.set({
-        //         replacement_mode:replacement_mode
-        //     },
-        //     function () {
-        //         getStorage();
-        //         // 向changeBackground.js发送消息以是新设置得以应用
-        //         sendMessageToContentScript({ cmd: "reload" });
-        //     });
-        sendMessageToContentScript({ cmd: "reload" });
-    });
-
-    // 跳转到教程页面
+    /** 跳转到教程页面 */
     $("#guide").on("click", function () {
         bg.goToGuidePage();
     });
